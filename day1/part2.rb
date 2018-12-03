a45944fe03a12db9b35d8a6382626783932dd6d7
Gabriel Gagnon <gabriel.gagnon@boldcommerce.com>
2018-12-03 13:15:19 -0600

    Refactor day 1

--- day1/part2.rb
+++ day1/part2.rb
@@ -1,14 +1,14 @@
 input = File.read('input1.txt')
 
-history = {0 => true}
+history = Set[0]
 memo = 0
 result = nil
 
 until result
   input.each_line.map(&:to_i).detect do |line|
     memo += line
-    result = memo if history.has_key? memo
-    history[memo] = true if result.nil?
+    result = memo if history.include? memo
+    history.add(memo) if result.nil?
     result
   end
 end
